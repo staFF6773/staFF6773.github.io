2ef3b43be70b29e576e12870d7f3fd52edf01dec
staFF6773 <108166164+staFF6773@users.noreply.github.com>
2024-11-21 17:33:35 -0600

    Create tailwind.config.js

---
+++ src/js/tailwind.config.js
@@ -0,0 +1,17 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                frieren: {
+                    light: '#e0f2f1',
+                    DEFAULT: '#26a69a',
+                    dark: '#00796b',
+                },
+            },
+            fontFamily: {
+                frieren: ['Quicksand', 'sans-serif'],
+            },
+        },
+    },
+}
